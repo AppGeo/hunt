8c70cadc46c2808885a74a155489d38c722a59cd
Ilya Radchenko <ilya@burstcreations.com>
2015-01-23 23:53:42 -0500

    Create new hunt

--- app/pods/new/route.js
+++ app/pods/new/route.js
@@ -1,4 +1,23 @@
 import Ember from 'ember';
+import DataRoute from 'ember-data-route/mixins/data-route';
 
-export default Ember.Route.extend({
+export default Ember.Route.extend(DataRoute, {
+  model: function () {
+    return this.store.createRecord('hunt');
+  },
+
+  willTransitionConfirm: function() {
+    return window.confirm('You have unsaved changes that will be lost. Do you want to continue?');
+  },
+
+  actions: {
+    create: function (model) {
+      model.save()
+        .then(function (data) {
+          console.log(data);
+        }, function (error) {
+          console.error(error);
+        });
+    }
+  }
 });
