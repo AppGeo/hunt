b76a5e2118d5a151d448c47258681d1d77608be1
Ishmael Smyrnow <ismyrnow@gmail.com>
2015-01-24 22:00:55 -0500

    Items done route
    - Copied logic from "new" route. Didn't test it.

--- app/pods/item/done/route.js
+++ app/pods/item/done/route.js
@@ -1,9 +1,39 @@
 import Ember from 'ember';
+import DataRoute from 'ember-data-route/mixins/data-route';
 
-export default Ember.Route.extend({
-
+export default Ember.Route.extend(DataRoute, {
   model: function () {
-    return this.store.createRecord('hunt');
-  }
+    var user = this.get('session.user');
+    var hunt = this.store.createRecord('hunt');
 
+    hunt.set('author', user);
+		hunt.set('items', window.items);
+
+    return hunt;
+  },
+
+  setupController: function (controller, model) {
+    this._super(controller, model);
+
+    var map = this.get('mapService.map');
+    map.addLayer(model.markers);
+  },
+
+  willTransitionConfirm: function () {
+    return window.confirm('You have unsaved changes that will be lost. Do you want to continue?');
+  },
+
+  actions: {
+    create: function (model) {
+      var self = this;
+
+      Ember.RSVP.all([model.get('items').invoke('save'), model.save()])
+        .then(function (data) {
+          console.log(data);
+          self.transitionTo('hunt', data[1].id);
+        }, function (error) {
+          console.error(error);
+        });
+    }
+  }
 });
